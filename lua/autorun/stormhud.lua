7d984d016eb0ef01ecbacfb824131f6be9802940
StormFusions <62036454+StormFusions@users.noreply.github.com>
2020-04-13 18:19:09 +0100

    Update stormhud.lua

--- lua/autorun/stormhud.lua
+++ lua/autorun/stormhud.lua
@@ -1,8 +1,10 @@
 if SERVER then
 	AddCSLuaFile("cl_hud.lua")
 	AddCSLuaFile("cl_hudmenu.lua")
+	AddCSLuaFile("vgui/stormvgui.lua")
 	include("sv_hud.lua")
 else
 	include("cl_hud.lua")
 	include("cl_hudmenu.lua")
+	include("vgui/stormvgui.lua")
 end
